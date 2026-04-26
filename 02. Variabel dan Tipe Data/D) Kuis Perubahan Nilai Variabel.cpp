79fd518a3b152e09c5dc01f65b82105036b8d256
Dede Iryawan <59034052+iryawan91@users.noreply.github.com>
2026-04-27 06:19:50 +0700

    Add files via upload

---
+++ 02. Variabel dan Tipe Data/D) Kuis Perubahan Nilai Variabel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 3;
+    x = x + 1;
+    cout << x << endl;
+    // 4   
+
+
+    int x = 3;
+    x = x + x;
+    cout << x << endl;
+    // 6
+
+    int x = 3;
+    int y = 4;
+    x = y;
+    y = x;
+    cout << x << " " << y << endl;   
+    //4 4
+
+
+}
+ 
---
+++ 02. Variabel dan Tipe Data/H) Rangkuman Variabel dan Tipe Data.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = x;
+    y = 5;
+    cout << x << endl;
+
+    // 10
+}
